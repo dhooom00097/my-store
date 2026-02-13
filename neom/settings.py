0dae0bf01b397fa46028713d32ddcbdf53f9a7b7
dhoom00097 <alharbidheem500@gmail.com>
2026-02-13 14:00:41 +0300

    Add whitenoise for production static

--- neom/settings.py
+++ neom/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 # ======================================
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # ← تمت إضافته
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -144,6 +145,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# ← تمت إضافته
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # ======================================
